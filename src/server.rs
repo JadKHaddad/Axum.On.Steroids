e6a091017747724db803f82062a1aa278770d27d
JadKHaddad <jadkhaddad@gmail.com>
2024-05-30 13:29:21 +0200

    added: shutdown_signal
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

---
+++ src/server.rs
@@ -0,0 +1,29 @@
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+
+        tracing::info!("CTRL+C received");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install SIGTERM signal handler")
+            .recv()
+            .await;
+
+        tracing::info!("SIGTERM received");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::info!("Shutting down");
+}
