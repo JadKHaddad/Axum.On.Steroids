c15393fc3419fd3e79ab082dfccac1bb8f735006
JadKHaddad <jadkhaddad@gmail.com>
2024-05-30 13:59:03 +0200

    added: ApiError
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

---
+++ src/error.rs
@@ -0,0 +1,42 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use derive_more::From;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, From)]
+#[serde(tag = "error_type", content = "error")]
+/// API error
+pub enum ApiError {
+    /// Internal server error
+    InternalServerError(InternalServerError),
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        match self {
+            ApiError::InternalServerError(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(self)).into_response()
+            }
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InternalServerError;
+
+/// Convert every error that implements [`core::convert::Into<>`] [`anyhow::Error`] into an [`InternalServerError`].
+impl<E> From<E> for InternalServerError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        let err: anyhow::Error = err.into();
+        let err = format!("{err:#}");
+        tracing::error!(%err, "Internal server error");
+
+        Self
+    }
+}
