0371ab0e7c367fed64c1fa5b256f190ee53fb322
JadKHaddad <jadkhaddad@gmail.com>
2024-06-01 10:45:32 +0200

    added: todo
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src/types/used_api_key.rs
+++ src/types/used_api_key.rs
@@ -1,3 +1,6 @@
+// TODO: this is being passed around like crazy and it's produced by ValidApiKey extractor and ApiKey extractor.
+// We don't know where it came from, so we don't know if it's valid or not.
+
 /// A struct to hold the used API key.
 ///
 /// Used to pass the API key arround as an Extension.
